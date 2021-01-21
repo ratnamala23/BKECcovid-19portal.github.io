e05cc6d5bb29f2f6913775c3552c22e933afe4d4
ratnamala23 <77729713+ratnamala23@users.noreply.github.com>
2021-01-21 21:54:00 +0530

    Update connect.php

--- artgallery/connect.php
+++ artgallery/connect.php
@@ -1,38 +1,4 @@
 <?php
-$username = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'password');
-if (!empty($name)){
-if (!empty($email)){
-$host = "localhost";
-$dbusername = "tube";
-$dbpassword = "hi";
-$dbname = "youtube";
-// Create connection
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-if (mysqli_connect_error()){
-die('Connect Error ('. mysqli_connect_errno() .') '
-. mysqli_connect_error());
-}
-else{
-$sql = "INSERT INTO account (name, comment)
-values ('$name','$comment')";
-if ($conn->query($sql)){
-echo "New record is inserted sucessfully";
-}
-else{
-echo "Error: ". $sql ."
-". $conn->error;
-}
-$conn->close();
-}
-}
-else{
-echo "comment should not  empty";
-die();
-}
-}
-else{
-echo "Username should not be empty";
-die();
+echo " connected successfully";
 }
 ?>
